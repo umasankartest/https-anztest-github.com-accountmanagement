12d811dc114f63f5601872ea542739ff77bdd7ae
umasankar potireddy <66990247+umasankartest@users.noreply.github.com>
2020-06-16 13:15:17 +0530

    Update AccountController.java

--- AccountManagement/src/main/java/com/anz/controller/AccountController.java
+++ AccountManagement/src/main/java/com/anz/controller/AccountController.java
@@ -4,15 +4,15 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.anz.dao.AccountRepository;
 import com.anz.dao.AccountTransactionRepository;
 import com.anz.model.AccountList;
 import com.anz.model.AccountTransaction;
 
+@RestController
 public class AccountController {
 
 	@Autowired
@@ -22,14 +22,12 @@ public class AccountController {
 	AccountTransactionRepository transactionRepo;
 		
 	@GetMapping("/accountList")
-	@ResponseBody
 	public Iterable<AccountList> getAccountList() {
 		
 		return repo.findAll();
 	}
 	
 	@GetMapping("/accountTransactions")
-	@ResponseBody
 	public Optional<AccountTransaction> getAccountTrnsactions(@RequestParam int id) {
 		
 	return transactionRepo.findById(id);
